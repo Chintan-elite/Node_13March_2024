9d848a9bcc8ce21edb19804bd75ed844308dc34f
chintan <chintan.tops@gmail.com>
2024-04-20 14:34:25 +0530

    mongoose

--- 007_mongoose/app.js
+++ 007_mongoose/app.js
@@ -6,7 +6,68 @@ const url = "mongodb://localhost:27017/student";
 mongoose.connect(url).then(data=>{
 
     console.log("db connected");
-
 }).catch(err=>{
-    console.log(err);
+ console.log(err);
 })
+
+const personalSchema = new mongoose.Schema({
+
+        name : {
+            type : String
+        },
+        email : {
+            type : String,
+            unique : true
+        },
+        age : {
+            type : Number
+        }
+})
+
+const Personal  = new mongoose.model("Personal",personalSchema)
+
+
+
+
+
+const addData = ()=>{
+
+    dt1 = {name:"test",email:"tech@gmail.com",age:25}
+    const per = new Personal(dt1)
+    per.save().then(data=>{
+        console.log(data);
+    }).catch(err=>{
+        console.log(err);
+    })
+
+}
+
+
+const addManydata = ()=>{
+
+    const d1 = {name:"tops",email:"tops11@gmail.com",age:10}
+    const d2 = {name:"test",email:"test11@gmail.com",age:25}
+    const d3 = {name:"abc",email:"abc11@gmail.com",age:20}
+
+    Personal.insertMany([new Personal(d1),new Personal(d2),new Personal(d3)]).then(data=>{
+        console.log(data);
+    }).catch(err=>{
+        console.log(err);
+    })
+
+}
+
+const viewData = ()=>{
+
+    Personal.find().then(data=>{
+        console.log(data);
+    }).catch(err=>{
+        console.log(err);
+    })
+
+}
+
+
+// addData()
+// addManydata()
+viewData()
